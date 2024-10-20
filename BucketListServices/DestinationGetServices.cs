af26ca42dd12ff58d5be66b57a10485658d7d7a6
lykasebelina <163415988+lykasebelina@users.noreply.github.com>
2024-10-20 08:20:54 +0800

    Add files via upload

---
+++ BucketListServices/DestinationGetServices.cs
@@ -0,0 +1,36 @@
+﻿using BucketListData;
+using BucketListModels;
+using System.Collections.Generic;
+
+namespace BucketListServices
+{
+    public class DestinationGetServices
+    {
+        SqlDbData sqlData = new SqlDbData();
+
+        public List<Destination> GetAllDestinations()
+        {
+            return sqlData.GetAllDestinations();
+        }
+
+        public Destination GetDestinationByName(string name)
+        {
+            return sqlData.GetDestinationByName(name);
+        }
+
+        public void AddNewDestination(Destination newDestination)
+        {
+            sqlData.AddNewDestination(newDestination);
+        }
+
+        public void DeleteDestination(string name)
+        {
+            sqlData.DeleteDestinationByName(name);
+        }
+
+        public void UpdateDestination(Destination updatedDestination)
+        {
+            sqlData.UpdateDestination(updatedDestination);
+        }
+    }
+} 
---
+++ BucketListServices/UserGetServices.cs
@@ -0,0 +1,14 @@
+﻿using BucketListData;
+
+namespace BucketListServices
+{
+    public class UserGetServices
+    {
+        SqlDbData sqlData = new SqlDbData();
+
+        public bool ValidateUser(string username, string password)
+        {
+            return sqlData.ValidateUser(username, password);
+        }
+    }
+} 
